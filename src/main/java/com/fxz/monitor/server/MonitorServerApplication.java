cafd84711f4e6958a4210e52e025ebc782d112ca
admin <fuxiuzhan>
2021-12-04 10:58:00 +0800

    init

---
+++ src/main/java/com/fxz/monitor/server/MonitorServerApplication.java
@@ -0,0 +1,13 @@
+package com.fxz.monitor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fxz/monitor/server/MonitorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxz.monitor.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
