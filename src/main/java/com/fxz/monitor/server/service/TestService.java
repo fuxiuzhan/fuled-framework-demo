4a12ce600c5f41916614e74023aff84904b8aa6c
admin <fuxiuzhan>
2021-12-06 22:50:36 +0800

    opts

--- src/main/java/com/fxz/monitor/server/service/TestService.java
+++ src/main/java/com/fxz/monitor/server/service/TestService.java
@@ -1,8 +1,10 @@
 package com.fxz.monitor.server.service;
 
 
+import com.fxz.fuled.config.starter.annotation.DimaondConfigChangeListener;
+import com.fxz.fuled.config.starter.model.ConfigChangeEvent;
 import com.fxz.fuled.logger.starter.annotation.Monitor;
-import com.fxz.fuled.simple.cache.Cache;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +12,23 @@ import org.springframework.stereotype.Service;
  * @author fxz
  */
 @Service
+@Slf4j
 public class TestService {
 
     @Value("${test.value:abc}")
     private String value;
 
-//    @Cache
+    //    @Cache
     @Monitor
     public String getInfo() {
         return value;
+    }
+
+
+    @DimaondConfigChangeListener(interestedKeys = "test.value", interestedKeyPrefixes = "test1")
+    public void configListener(ConfigChangeEvent event) {
+        event.changedKeys().forEach(k -> {
+            log.info("config changes namespace->{},key->{},value->{}", event.getNamespace(), k, event.getChange(k));
+        });
     }
 }
